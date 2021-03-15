28fca4bdb595e6d691f1f5dc80381de5da569db4
Osama Azbarga <osamaazbarga@gmail.com>
2021-03-15 13:26:58 +0200

    7.1-increment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Button1 from './Components/Buttons-4.1/Buttons-4.1';
 import Cards from './Components/Cards-4.2/card'
+import Increment from './Components/Increment-7.1/Button'
+
 import faker from 'faker'
 
 
@@ -10,9 +12,12 @@ function App() {
     <div className="App" >
       {/* <Button1 newbtn="Important" stylebtn='bold'/>
       <Button1 newbtn="Not Important"/> */}
+
+      {/* <Cards linktext1="SHARE" linktext2="EXPLORE"/>
       <Cards linktext1="SHARE" linktext2="EXPLORE"/>
-      <Cards linktext1="SHARE" linktext2="EXPLORE"/>
-      <Cards linktext1="SHARE" linktext2="EXPLORE"/>
+      <Cards linktext1="SHARE" linktext2="EXPLORE"/> */}
+
+      <Increment/>
 
       
     </div>
---
+++ src/Components/Increment-7.1/Button.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class Countbtn extends Component{
+    constructor(){
+        super();
+        this.state={count:0}
+        this.buttonClicked = this.buttonClicked.bind(this);
+    }
+    
+
+    buttonClicked(e) {
+        this.setState({count: this.state.count+1});
+    }
+    render(){
+        return (
+    
+            <div>
+                <button onClick={this.buttonClicked}>
+                {
+                    'increment'
+                }
+            </button>
+            {this.state.count}
+            </div>
+            );
+    }
+}
+
+
+
+export default Countbtn;
